e92578c7fe83787a7c622f879cf6c384cd651fc6
solid0x <solidz0x@gmail.com>
2024-02-21 15:32:52 +0700

    Black formatting

--- app/src/settings.py
+++ app/src/settings.py
@@ -2,7 +2,9 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:admin@postgres/currency_converter_db"
+    DATABASE_URL: str = (
+        "postgresql+asyncpg://postgres:admin@postgres/currency_converter_db"
+    )
     API_KEY: str = "aa5b14f1fb6f4ce8c50c41c3"
 
 
